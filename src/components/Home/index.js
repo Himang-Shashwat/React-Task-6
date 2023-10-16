e2bd601af02df3860b5afb7a0ecb82c7f693643b
Himang Shashwat <himangshashwat10@gmail.com>
2023-10-16 18:42:47 +0530

    Task-6 Portfolio

---
+++ src/components/Home/index.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = [' ','H','i', 'm', 'a', 'n', 'g', ' ', 'S', 'h', 'a','s','h','w','a','t']
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+          </h1>
+          <h2>Front End Learner / React / Tech-Enthusiast</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
